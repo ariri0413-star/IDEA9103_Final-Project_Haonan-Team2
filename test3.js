3727ddfe67e686b3f0471b868f5a8ac484075090
Yang-Zhou-123 <yzho0853@uni.sydney.edu.au>
2026-05-29 14:46:44 +1000

    Nun

--- test3.js
+++ test3.js
@@ -1,8 +1,10 @@
 let img;
 
+let bgMask = [];
+
 function preload() {
 
-  img = loadImage("image/nun.png");
+  img = loadImage("image/nun1.png");
 
 }
 
@@ -14,25 +16,25 @@ function setup() {
 
   pixelDensity(1);
 
+  img.loadPixels();
+
+  createBackgroundMask();
+
 }
 
 function draw() {
 
-  clear(); 
+  clear(); // Transparent background
 
-  // If you want to see the transparent area while testing, use:
+  let pixelSize = 4; // Smaller = more detailed pixel style
 
-  // background(230);
-
-  let pixelSize = 6; // Smaller number = finer pixel style
-
-  let displayH = height * 0.85;
+  let displayH = height * 0.9;
 
   let displayW = displayH * (img.width / img.height);
 
-  if (displayW > width * 0.85) {
+  if (displayW > width * 0.9) {
 
-    displayW = width * 0.85;
+    displayW = width * 0.9;
 
     displayH = displayW * (img.height / img.width);
 
@@ -42,8 +44,6 @@ function draw() {
 
   let startY = height / 2 - displayH / 2;
 
-  img.loadPixels();
-
   for (let y = 0; y < displayH; y += pixelSize) {
 
     for (let x = 0; x < displayW; x += pixelSize) {
@@ -52,7 +52,17 @@ function draw() {
 
       let imgY = floor(map(y, 0, displayH, 0, img.height));
 
-      let index = (imgY * img.width + imgX) * 4;
+      let id = imgY * img.width + imgX;
+
+      // Only remove background pixels connected to the image edge
+
+      if (bgMask[id]) {
+
+        continue;
+
+      }
+
+      let index = id * 4;
 
       let r = img.pixels[index];
 
@@ -61,14 +71,6 @@ function draw() {
       let b = img.pixels[index + 2];
 
       let a = img.pixels[index + 3];
-
-      // Remove very light background
-
-      if (r > 190 && g > 190 && b > 190) {
-
-        continue;
-
-      }
 
       noStroke();
 
@@ -79,6 +81,78 @@ function draw() {
     }
 
   }
+
+}
+
+function createBackgroundMask() {
+
+  let w = img.width;
+
+  let h = img.height;
+
+  bgMask = new Array(w * h).fill(false);
+
+  let visited = new Array(w * h).fill(false);
+
+  let queue = [];
+
+  // Use the four corners as background starting points
+
+  queue.push([0, 0]);
+
+  queue.push([w - 1, 0]);
+
+  queue.push([0, h - 1]);
+
+  queue.push([w - 1, h - 1]);
+
+  while (queue.length > 0) {
+
+    let [x, y] = queue.shift();
+
+    if (x < 0 || x >= w || y < 0 || y >= h) continue;
+
+    let id = y * w + x;
+
+    if (visited[id]) continue;
+
+    visited[id] = true;
+
+    if (isBackgroundPixel(x, y)) {
+
+      bgMask[id] = true;
+
+      queue.push([x + 1, y]);
+
+      queue.push([x - 1, y]);
+
+      queue.push([x, y + 1]);
+
+      queue.push([x, y - 1]);
+
+    }
+
+  }
+
+}
+
+function isBackgroundPixel(x, y) {
+
+  let index = (y * img.width + x) * 4;
+
+  let r = img.pixels[index];
+
+  let g = img.pixels[index + 1];
+
+  let b = img.pixels[index + 2];
+
+  // Background is light grey, but this only applies to connected edge areas
+
+  let brightness = (r + g + b) / 3;
+
+  let colorDifference = abs(r - g) + abs(g - b) + abs(r - b);
+
+  return brightness > 120 && colorDifference < 55;
 
 }
 
